729d61aee85ef9d65921b96242ad898433255b0c
Asimina Athanasopoulou <asimina.ath@outlook.com>
2025-02-24 13:07:29 +0000

    Create initial Java files

--- src/main/java/com/example/ToDoBread/ToDoBreadController.java
+++ src/main/java/com/example/ToDoBread/ToDoBreadController.java
@@ -36,11 +36,11 @@ can interact with the repository to perform CRUD operations on Todo entities.
 
 The class defines two endpoint methods:
 
-getTodos(): This method is mapped to HTTP GET requests at the /todos URL. 
+1. getTodos(): This method is mapped to HTTP GET requests at the /todos URL. 
 It uses the @GetMapping annotation and returns a list of all Todo entities by 
 calling the findAll() method of the ToDoRepository. The returned list is cast to List<Todo>.
 
-createTodo(@RequestBody Todo todo): This method is mapped to HTTP POST requests 
+2. createTodo(@RequestBody Todo todo): This method is mapped to HTTP POST requests 
 at the /todos URL. It uses the @PostMapping annotation and accepts a Todo object 
 in the request body, indicated by the @RequestBody annotation. 
 The method saves the provided Todo entity to the database by calling the save() method 
